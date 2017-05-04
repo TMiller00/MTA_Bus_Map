376c3a1102fbd0a329dae4209f93401b167a789f
TMiller00 <thomas.d.miller.jr@gmail.com>
2017-05-03 21:46:07 -0400

    Initial commit

---
+++ src/lib/busService.js
@@ -0,0 +1,27 @@
+const baseURL = 'http://localhost:5000/'
+
+export const vehicleService = () => {
+  return fetch(baseURL)
+    .then(res => res.json())
+    .then(json => {
+      const temp = []
+      const timestamp = json.Siri.ServiceDelivery.ResponseTimestamp
+      const busData = json.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity
+      busData.forEach(bus => {
+        if('MonitoredCall' in bus.MonitoredVehicleJourney) {
+          const base = bus.MonitoredVehicleJourney
+          temp.push(
+            {
+              id: base.VehicleRef.split('_')[1],
+              lat: base.VehicleLocation.Latitude,
+              lng: base.VehicleLocation.Longitude,
+              line: base.PublishedLineName[0],
+              destination: base.DestinationName[0],
+              onwardCalls: base.OnwardCalls.OnwardCall
+            }
+          )
+        }
+      })
+      return [timestamp, temp]
+    })
+}
