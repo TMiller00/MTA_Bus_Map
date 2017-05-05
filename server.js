3a344a00fa867d7d99a30f62109571448caeb1e9
TMiller00 <thomas.d.miller.jr@gmail.com>
2017-05-05 08:02:40 -0400

    test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
 }
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
